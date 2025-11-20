e034c38953ffc567034b2a239c7042f12e608aa7
RichardMac <richkejie@gmail.com>
2025-11-20 12:28:46 -0500

    use robotics toolbox structures

--- Lab4/code/att.m
+++ Lab4/code/att.m
@@ -10,19 +10,19 @@ function tau = att(q,q2,myrobot)
 
         % extract transformation
         H_i = myrobot.A(1:i, q);
-        o_i = H_i(1:3,4); % origin of Fi wrt F0
+        o_i = H_i.t; % origin of Fi wrt F0
 
         % fill columns of J until column i
         for j = 1:i
             H_j_1 = myrobot.A(1:j-1,q);
-            R_j_1 = H_j_1(1:3,1:3);
+            R_j_1 = H_j_1.R;
             z_j_1 = R_j_1(:,3); % z_{j-1} wrt F0
-            o_j_1 = H_j_1(1:3,4); % origin of F{j-1} wrt F0
+            o_j_1 = H_j_1.t; % origin of F{j-1} wrt F0
             J_v_i(:,j) = cross(z_j_1, (o_i-o_j_1)); % column
         end
         
         H_i_f = myrobot.A(1:i, q2);
-        o_i_f = H_i_f(1:3,4); % final joint angle
+        o_i_f = H_i_f.t; % final joint angle
         
         zeta_i = 1; % assume scaling constant is just 1 (can modify later)
         F_i_att = -zeta_i*(o_i - o_i_f);
